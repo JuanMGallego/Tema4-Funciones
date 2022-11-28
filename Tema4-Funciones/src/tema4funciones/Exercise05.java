2f283b9274b1012505d205299ee7fe0a6313d365
jmgallego <jmgallego@106-09.info2>
2022-11-28 11:14:07 +0100

    First commit

--- Tema4-Funciones/src/tema4funciones/Exercise05.java
+++ Tema4-Funciones/src/tema4funciones/Exercise05.java
@@ -1,11 +1,36 @@
 package tema4funciones;
 
+import java.util.Scanner;
+
 public class Exercise05 {
 
+	public static boolean vocal(String letter) {
+		
+		boolean isVocal;
+		
+		isVocal = letter.equals("a")||letter.equals("e")||letter.equals("i")||letter.equals("o")||letter.equals("u")? true : false;
+		
+		return isVocal;
+			
+	}
+	
 	public static void main(String[] args) {
 
+		String letter;
+		boolean isVocal;
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduzca una letra: ");
+		letter = sc.nextLine();
+		System.out.println();
+		
+		isVocal = vocal(letter);
+		
+		System.out.println("¿Es vocal?: " + isVocal);
+		
+		sc.close();
 
 	}
-
+	
 }
